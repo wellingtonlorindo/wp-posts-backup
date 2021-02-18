96db48a307ec1afbe60744f7149701d29d3e4b1e
wellington lorindo <wellington.lorindo@monetizze.com.br>
2021-02-17 23:42:07 -0300

    Add api automated tests

---
+++ tests/Feature/ApiPostsTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Post;
+
+class ApiPostsTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory('App\User')->create([
+            'api_token' => 'owIydznBTNnRsnnBxS8UuTf3kbRolFrOehUartCuVOD4V92qTflJCk6fE2Zn'
+        ]);
+    }
+
+    public function testUserCanAddAPost()
+    {
+        $post = [
+            'post_id' => 16,
+            'post_title' => 'My first post',
+            'post_content' => 'The content here'
+        ];
+
+        $response = $this->withHeaders([
+            'token' => $this->user->api_token,
+        ])->json('POST', '/api/posts', $post);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => "success",
+            ]);
+    }
+
+    public function testUserCantAddAPost()
+    {
+        $post = [
+            'post_id' => 111,
+            'post_title' => 'My post',
+            'post_content' => 'The content here'
+        ];
+
+        $response = $this->withHeaders([
+            'token' => null,
+        ])->json('POST', '/api/posts', $post);
+
+        $response
+            ->assertStatus(401)
+            ->assertJson([
+                'message' => "Unauthenticated.",
+            ]);
+
+        $response = $this->withHeaders([
+            'token' => 'aaaaaaaaaaaaaaaaaaaaaaaa',
+        ])->json('POST', '/api/posts', $post);
+
+        $response
+            ->assertStatus(401)
+            ->assertJson([
+                'message' => "Unauthenticated.",
+            ]);
+    }
+
+    public function testUserCanReadThePosts()
+    {
+        $post = factory('App\Post')->create(['user_id' => $this->user->id]);
+        $response = $this->withHeaders([
+            'token' => $this->user->api_token,
+        ])->json('GET', '/api/posts');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => "success",
+            ]);
+
+        $response = $this->withHeaders([
+            'token' => $this->user->api_token,
+        ])->json('GET', '/api/posts/'.$post->id);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => "success",
+            ]);
+    }
+
+    public function testUserCantReadThePosts()
+    {
+        $anotherUser = factory('App\User')->create();
+        $post = factory('App\Post')->create(['user_id' => $anotherUser->id]);
+        $response = $this->withHeaders([
+            'token' => $this->user->api_token,
+        ])->json('GET', '/api/posts');
+
+        $response
+            ->assertStatus(200)
+            ->assertDontSee($post->post_title)
+            ->assertJson([
+                'status' => "success",
+            ]);
+
+        $response = $this->withHeaders([
+            'token' => $this->user->api_token,
+        ])->json('GET', '/api/posts/'.$post->id);
+
+        $response
+            ->assertStatus(403)
+            ->assertDontSee($post->post_title);
+    }
+}
